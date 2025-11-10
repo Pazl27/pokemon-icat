4ac4ef04dc1539aed0ca97483f62c9aaca0cb6b5
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-11-10 20:49:13 +0100

    flake for nixos integration
    added a flake that makes it possible to integrate it to nixos and run it
    with nix

--- flake.nix
+++ flake.nix
@@ -13,68 +13,139 @@
           inherit system;
         };
 
-        packages = pkgs.callPackage ./default.nix {};
-      in {
-        packages.default = packages.pokemon-icat;
+        # Python environment with required dependencies
+        pythonWithDeps = pkgs.python3.withPackages (ps: with ps; [
+          aiohappyeyeballs
+          aiohttp
+          aiosignal
+          async-timeout
+          attrs
+          frozenlist
+          idna
+          markdown-it-py
+          mdurl
+          multidict
+          pillow
+          propcache
+          pygments
+          python-slugify
+          rich
+          text-unidecode
+          typing-extensions
+          yarl
+        ]);
 
-        # Optional: add devShell
-        # devShell = pkgs.mkShell {
-        #   buildInputs = with pkgs; [
-        #     rustc
-        #     cargo
-        #     pkg-config
-        #     python3
-        #     python3Packages.pillow
-        #     # any other dev deps you want
-        #   ];
-        # };
+        # Download and prepare Pokemon icons
+        pokemon-icons = pkgs.stdenv.mkDerivation {
+          pname = "pokemon-icons";
+          version = "1.2.0";
+
+          src = pkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = pkgs.lib.fileset.unions [
+              ./setup_icons.py
+              ./bin
+            ];
+          };
+
+          nativeBuildInputs = [
+            pythonWithDeps
+            pkgs.cacert
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            pkgs.optipng
+          ];
+
+          # Fixed output derivation to cache the downloaded icons
+          outputHashMode = "recursive";
+          outputHashAlgo = "sha256";
+          outputHash = "sha256-ntz0i9HWuJXeGRO7JX/wMsjiHcpmNv/HImgqeMN4YQ8=";
+
+          buildPhase = ''
+            export POKEMON_ICAT_DATA=$TMPDIR
+            export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+
+            mkdir -p $POKEMON_ICAT_DATA/pokemon-icons/normal
+            mkdir -p $POKEMON_ICAT_DATA/pokemon-icons/shiny
+
+            python3 setup_icons.py --upscale 3
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r $POKEMON_ICAT_DATA/pokemon-icons $out/
+          '';
+        };
+
+        # Main pokemon-icat package
+        pokemon-icat = pkgs.rustPlatform.buildRustPackage {
+          pname = "pokemon-icat";
+          version = "1.2.0";
+
+          src = pkgs.lib.cleanSource ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [
+            pkgs.makeWrapper
+          ];
+
+          postInstall = ''
+            # Create the data directory structure
+            mkdir -p $out/share/pokemon-icat
+
+            # Copy the CSV data file and Python modules
+            cp -r bin/* $out/share/pokemon-icat/
+
+            # Copy the downloaded Pokemon icons
+            cp -r ${pokemon-icons}/pokemon-icons $out/share/pokemon-icat/
+
+            # Wrap the binary to set the POKEMON_ICAT_DATA environment variable
+            wrapProgram $out/bin/pokemon-icat \
+              --set POKEMON_ICAT_DATA $out/share/pokemon-icat
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Show Pokémons inside your terminal!";
+            homepage = "https://github.com/aflaag/pokemon-icat";
+            license = licenses.mit;
+            maintainers = [ ];
+            mainProgram = "pokemon-icat";
+          };
+        };
+
+      in {
+        packages = {
+          default = pokemon-icat;
+          pokemon-icat = pokemon-icat;
+          pokemon-icons = pokemon-icons;
+        };
+
+        # For development
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            pkg-config
+            pythonWithDeps
+          ];
+
+          shellHook = ''
+            export POKEMON_ICAT_DATA="$PWD/.local/share/pokemon-icat"
+            mkdir -p "$POKEMON_ICAT_DATA"
+          '';
+        };
+
+        # Make the app runnable with `nix run`
+        apps.default = {
+          type = "app";
+          program = "${pokemon-icat}/bin/pokemon-icat";
+          meta = {
+            description = "Show Pokémons inside your terminal!";
+            mainProgram = "pokemon-icat";
+          };
+        };
       }
     );
 }
-
-
-# {
-#   description = "your description here";
-#
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # or whatever you use
-#     flake-utils.url = "github:numtide/flake-utils";
-#   };
-#
-# outputs = { self, nixpkgs, flake-utils }:
-#   flake-utils.lib.eachDefaultSystem (system:
-#     let
-#       pkgs = import nixpkgs { inherit system; };
-#
-#       src = pkgs.lib.cleanSourceWith {
-#         src = ./.;
-#         filter = path: type:
-#           let baseName = baseNameOf path;
-#           in baseName != ".git"
-#              && baseName != "target"
-#              && baseName != "result"
-#              && baseName != ".direnv";
-#       };
-#
-#       packages = pkgs.callPackage ./default.nix {
-#         inherit src;
-#       };
-#     in {
-#       packages.default = packages.pokemon-icat;
-#       # packages.pokemon-icons = packages.pokemon-icons;
-#
-#       # devShell = pkgs.mkShell {
-#       #   buildInputs = with pkgs; [
-#       #     rustc
-#       #     cargo
-#       #     pkg-config
-#       #     python3
-#       #     python3Packages.pillow
-#       #   ];
-#       # };
-#     }
-#   );
-#
-# }
-
-
